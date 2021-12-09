da9b731deb37d584aaa56fca8cf3f198ef9627aa
SuzannePS <suzannepatriciadesouza@yhaoo.com.br>
2021-12-09 13:30:33 -0300

    versao 1

---
+++ base64.js
@@ -0,0 +1,12 @@
+function base64(){
+    var mensagem = document.querySelector("#mensagem").value 
+    if(document.querySelector("#cod1").checked){
+        event.preventDefault()
+        var resposta = btoa(mensagem)
+        document.querySelector("#resultado").innerHTML= resposta
+    }else if(document.querySelector("#cod2").checked) {
+        event.preventDefault()
+        var resposta = atob(mensagem)
+        document.querySelector("#resultado").innerHTML= resposta
+    }
+} 
---
+++ global.js
@@ -0,0 +1,6 @@
+function criptografia(){
+    if(document.querySelector("#opcoes").value == "base"){
+        base64()
+    }else if (document.querySelector("#opcoes").value == "cifra"){
+    }
+}
